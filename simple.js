6258d2e00bd90a03792ec095b3cb514d511e4372
abigailleon99 <58637906+abigailleon99@users.noreply.github.com>
2020-01-15 11:53:08 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,40 +1,102 @@
+//change background to skyblue
 d3.select('svg')
-.style('background-color', 'skyblue')
+    .style('background-color', 'skyblue')
+
 //adding a rectangle with red border
 d3.select('svg').append("rect")
             .attr("x", 0)
             .attr("y", 0)
-            //dont know how to do the red border
             .attr("width", 600)
             .attr("height", 200)
-            .style('color', 'red')
+            //red border
+            .style('stroke', 'red')
+            .style('stroke-width', '5px')
+            .style('fill', 'white')
 
-//Changing the h2 element
+//changing the h2 element
 d3.select('h2')
-.text('A really simple description')
+    .text('A really simple description')
+    .style('color', 'orange')
 
-//dont know how to change the color of the circle
-//I just changed it in the html file
+//changing the second header 2
+
+//change the color of the circle -- I just changed it in the html file
 
 //add a text to svg
 d3.select('svg').append('text')
-.attr('x',300)
-.attr('y',100)
-.text('Hello! New text here.')
+    .attr('x',300)
+    .attr('y',100)
+    .text('Hello! New text here.')
+
 //remove the first text from svg
-d3.select('text').remove()
+d3.select('text')
+    .remove()
+
 //add circle
 d3.select('svg').append("circle")
-.attr('r', 40)
-.attr('cx', 200)
-.attr('cy', 150)
-.attr('fill', white)
+    .attr('r', 40)
+    .attr('cx', 200)
+    .attr('cy', 150)
+    .attr('fill', 'yellow')
 
+//changing the opacity
 d3.selectAll('svg')
-.style('opacity', 0.3)
+    .style('opacity', 0.3)
+
+
 
 //circle for loop
-
 for (let i = 0; i < 10; i++){
-    d3.select('svg').append("circle")
+    d3.select('svg')
+    .append("circle")
+    .attr('x', 50 )
 }
+
+//rectangle for loop
+for (var i = 0; i < 6; i++){
+    let space = 20
+    if(i/2%1){
+         d3.select('svg')
+        .append('rect')
+        .attr("x", i * space)
+        .attr("y", 0)
+        .attr("width", 20)
+        .attr("height", 20)
+    }else{
+        d3.select('svg')
+        .append('rect')
+        // .attr('id', 'greenbox')
+        .attr("x", i * space)
+        .attr("y", 0)
+        .attr("width", 20)
+        .attr("height", 20)
+        .style('fill', 'green')
+        
+    }
+}
+
+//second header
+d3.select('body')
+    .append('h2')
+    .text('My second picture')
+
+//Chessboard
+//SVG defined.style('opacity', 0.2)
+d3.select('body')
+    .append('svg')
+    .attr('id', 'chessboard')
+    .attr('width', 300)
+    .attr('height', 300)
+    .style('background-color', 'white')
+
+    //chess squares
+    for (var i = 0; i < 4; i++){
+        let space = 20;
+        d3.select('svg#chessboard')
+            .append('rect')
+            .attr( 'x', i*space)
+            .attr( 'y', 0)
+            .attr('width', 20)
+            .attr('height', 20)
+        // if (i/3)
+    }
